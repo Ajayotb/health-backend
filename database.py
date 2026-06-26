b70609a6b69aeae14eea4666ca9303fdbaa80895
Ajayotb <joseadetokunbo@gmail.com>
2026-06-26 09:03:19 +0100

    Fix requirements.txt for Linux deployment

--- database.py
+++ database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
 
-DATABASE_URL = "sqlite:///./health_data.db"
+import os
+DATABASE_URL = f"sqlite:///{os.path.join(os.getcwd(), 'health_data.db')}"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
